c691e4265bfda3bae230bffce449df8d1c3be448
JOY DEEP SAHA <88198830+sahajoydeep2002@users.noreply.github.com>
2021-11-13 21:09:46 +0530

    commiting crimes

---
+++ shaders.cpp
@@ -0,0 +1,203 @@
+#include "shaders.h"
+
+#include <glm/gtc/matrix_transform.hpp>
+
+shader::shader(const char* vertexPath, const char* fragmentPath, const char* geometryPath)
+{
+    // 1. retrieve the vertex/fragment source code from filePath
+    std::string vertexCode;
+    std::string fragmentCode;
+    std::string geometryCode;
+    std::ifstream vShaderFile;
+    std::ifstream fShaderFile;
+    std::ifstream gShaderFile;
+    // ensure ifstream objects can throw exceptions:
+    vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    gShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    try
+    {
+        // open files
+        vShaderFile.open(vertexPath);
+        fShaderFile.open(fragmentPath);
+        std::stringstream vShaderStream, fShaderStream;
+        // read file's buffer contents into streams
+        vShaderStream << vShaderFile.rdbuf();
+        fShaderStream << fShaderFile.rdbuf();
+        // close file handlers
+        vShaderFile.close();
+        fShaderFile.close();
+        // convert stream into string
+        vertexCode = vShaderStream.str();
+        fragmentCode = fShaderStream.str();
+
+        if (geometryPath != nullptr)
+        {
+            gShaderFile.open(geometryPath);
+            std::stringstream gShaderStream;
+            gShaderStream << gShaderFile.rdbuf();
+            gShaderFile.close();
+            geometryCode = gShaderStream.str();
+        }
+    }
+    catch (std::ifstream::failure&)
+    {
+        std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
+    }
+    const char* vShaderCode = vertexCode.c_str();
+    const char* fShaderCode = fragmentCode.c_str();
+    // 2. compile shaders
+    unsigned int vertex, fragment;
+    // vertex shader
+    vertex = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex, 1, &vShaderCode, NULL);
+    glCompileShader(vertex);
+    checkCompileErrors(vertex, "VERTEX");
+    // fragment Shader
+    fragment = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment, 1, &fShaderCode, NULL);
+    glCompileShader(fragment);
+    checkCompileErrors(fragment, "FRAGMENT");
+
+    unsigned int geometry;
+    if (geometryPath != nullptr)
+    {
+        const char* gShaderCode = geometryCode.c_str();
+        geometry = glCreateShader(GL_GEOMETRY_SHADER);
+        glShaderSource(geometry, 1, &gShaderCode, NULL);
+        glCompileShader(geometry);
+        checkCompileErrors(geometry, "GEOMETRY");
+    }
+    // shader Program
+    ID = glCreateProgram();
+    glAttachShader(ID, vertex);
+    glAttachShader(ID, fragment);
+    if (geometryPath != nullptr)
+        glAttachShader(ID, geometry);
+    glLinkProgram(ID);
+    checkCompileErrors(ID, "PROGRAM");
+    // delete the shaders as they're linked into our program now and no longer necessery
+    glDeleteShader(vertex);
+    glDeleteShader(fragment);
+    if (geometryPath != nullptr)
+        glDeleteShader(geometry);
+}
+
+void shader::use()
+{
+    glUseProgram(ID);
+}
+// utility uniform functions
+
+void shader::setBool(const std::string& name, bool value)
+{
+    glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
+}
+
+void shader::setInt(const std::string& name, int value)
+{
+    glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+}
+
+void shader::setFloat(const std::string& name, float value)
+{
+    glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+}
+
+void shader::setVec2(const std::string& name, const glm::vec2& value)
+{
+    glUniform2fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+}
+
+void shader::setVec2(const std::string& name, float x, float y)
+{
+    glUniform2f(glGetUniformLocation(ID, name.c_str()), x, y);
+}
+
+void shader::setVec3(const std::string& name, const glm::vec3& value)
+{
+    glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+}
+
+void shader::setVec3(const std::string& name, float x, float y, float z)
+{
+    glUniform3f(glGetUniformLocation(ID, name.c_str()), x, y, z);
+}
+
+void shader::setVec4(const std::string& name, const glm::vec4& value)
+{
+    glUniform4fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
+}
+
+void shader::setVec4(const std::string& name, float x, float y, float z, float w)
+{
+    glUniform4f(glGetUniformLocation(ID, name.c_str()), x, y, z, w);
+}
+
+void shader::setMat2(const std::string& name, const glm::mat2& mat)
+{
+    glUniformMatrix2fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+}
+
+void shader::setMat3(const std::string& name, const glm::mat3& mat)
+{
+    glUniformMatrix3fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+}
+
+void shader::setMat4(const std::string& name, const glm::mat4& mat)
+{
+    glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+}
+
+void shader::checkCompileErrors(GLuint shdr, std::string type)
+{
+    GLint success;
+    GLchar infoLog[1024];
+    if (type != "PROGRAM")
+    {
+        glGetShaderiv(shdr, GL_COMPILE_STATUS, &success);
+        if (!success)
+        {
+            glGetShaderInfoLog(shdr, 1024, NULL, infoLog);
+            std::cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+        }
+    }
+    else
+    {
+        glGetProgramiv(shdr, GL_LINK_STATUS, &success);
+        if (!success)
+        {
+            glGetProgramInfoLog(shdr, 1024, NULL, infoLog);
+            std::cout << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+        }
+    }
+}
+
+std::map<std::string, shader> shaders;
+bool compileShaders()
+{
+	bool success = true;
+	glm::mat4 projection = glm::ortho(0.0f, (float)1280, (float)720, 0.0f, -5.0f, 5.0f);
+
+    success &= shaders.insert(std::pair<std::string, shader>("animation", shader("shaders/animation.vs", "shaders/animation.fs"))).second;
+    success &= shaders.insert(std::pair<std::string, shader>("box", shader("shaders/box.vs", "shaders/box.fs"))).second;
+	success &= shaders.insert(std::pair<std::string, shader>("image", shader("shaders/image.vs", "shaders/image.fs"))).second;
+    success &= shaders.insert(std::pair<std::string, shader>("screen", shader("shaders/screen.vs", "shaders/screen.fs"))).second;
+	success &= shaders.insert(std::pair<std::string, shader>("text", shader("shaders/text.vs", "shaders/text.fs"))).second;
+
+    shaders.at("animation").use();
+    shaders.at("animation").setMat4("projection", projection);
+    shaders.at("box").use();
+    shaders.at("box").setMat4("projection", projection);
+	shaders.at("screen").use();
+	shaders.at("screen").setMat4("projection", projection);
+	shaders.at("image").use();
+	shaders.at("image").setMat4("projection", projection);
+
+	return success;
+}
+
+std::map<std::string, shader>* getShaders()
+{
+	return &shaders;
+}
